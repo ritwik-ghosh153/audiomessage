d6e24fb04ffedd07a248d23933be678dd44e6474
ritwik_ghosh153 <ritwik.ghosh1010@gmail.com>
2020-05-26 12:22:20 +0530

    changed ui

---
+++ android/app/src/main/kotlin/com/ghosh/audiomessage/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ghosh.audiomessage
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -26,75 +26,96 @@ class _VoiceNoteState extends State<VoiceNote> {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 30),
-                child: SizedBox(
-                  height: 50,
-                  child: Visibility(
-                    visible: isRecording,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.blue[700],
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                      child: Center(
-                          child: Text('Recording...',
-                          style: TextStyle(
-                            color: Colors.white
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(0.0),
+                    child: SizedBox(
+                      height: 30,
+                      width: 180,
+                      child: Visibility(
+                        visible: isRecording,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.grey[400],
+                            borderRadius: BorderRadius.only(topLeft: Radius.circular(30),bottomLeft: Radius.circular(30),),
                           ),
-                          )
+                          child: Center(
+                              child: Row(
+                                children: <Widget>[
+                                  Icon(Icons.mic, color: Colors.red,),
+                                  Text('<<< swipe to cancel',
+                                    style: TextStyle(
+                                        color: Colors.white
+                                    ),
+                                  ),
+                                ],
+                              )
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  GestureDetector(
-                    child: Icon(Icons.mic, size: 50, color: _micColor,),
-                    onLongPressStart: (details)async{
-                      isRecording=true;
-                      name=DateTime.now().toIso8601String();
-                      save=Save(name,null);
-                      await save.start();
-                      setState(() {
-                        _micColor=Colors.black;
-                      });
-                    },
-                    onLongPressEnd: (details)async{
-                      isRecording=false;
-                      await save.stop();
-                      final path=save.getPath();
-                      duration=save.getDuration().toString();
-                      upload=Upload();
-                      upload.uploadFile(File(path), name);
-                      //TODO: retrieve this recording at path and push it to firebase
-                      var record=File(path);
-                      print(record);
-                      setState(() {
-                        _micColor=Colors.grey;
-                      });
-                    },
+                  Builder(
+                    builder: (context) => GestureDetector(
+                      child: Icon(Icons.mic, size: 50, color: _micColor,),
+                      onLongPressStart: (details)async{
+                        isRecording=true;
+                        name=DateTime.now().toIso8601String();
+                        save=Save(name,null);
+                        await save.start();
+                        setState(() {
+                          _micColor=Colors.black;
+                        });
+                      },
+                      onLongPressEnd: (details)async{
+                        if(details.localPosition.dx <-80){
+                          isRecording = false;
+                          await save.stop();
+                          final path = save.getPath();
+                          print(details.localPosition.dx);
+                          setState(() {
+                            _micColor = Colors.grey;
+                          });
+                          Scaffold.of(context).showSnackBar(SnackBar(content: Text('Recording cancelled'),duration: Duration(seconds: 2),));
+                        }
+                        else {
+                          isRecording = false;
+                          await save.stop();
+                          final path = save.getPath();
+                          duration = save.getDuration().toString();
+                          upload = Upload();
+                          upload.uploadFile(File(path), name);
+                          //TODO: retrieve this recording at path and push it to firebase
+                          var record = File(path);
+                          print(record);
+                          print(details.localPosition.dx);
+                          setState(() {
+                            _micColor = Colors.grey;
+                          });
+                          Scaffold.of(context).showSnackBar(SnackBar(content: Text('Recording saved'),duration: Duration(seconds: 2),));
+                        }
+                      },
+                    ),
                   ),
-                  GestureDetector(
-                    child: Icon(Icons.play_arrow, size: 50, color: Colors.black,),
-                    onTap: ()async{
-                      setState(() {
-                        play=Icon(Icons.pause, size: 50, color: Colors.black,);
-                      });
-                      print('ritwik'+upload.audioURL);
-                      await player.play(upload.audioURL);
-                      setState(() {
-                        play=play=Icon(Icons.play_arrow, size: 50, color: Colors.black,);
-                      });
-                    },
-                  )
                 ],
               ),
+              GestureDetector(
+                child: Icon(Icons.play_arrow, size: 50, color: Colors.black,),
+                onTap: ()async{
+                  setState(() {
+                    play=Icon(Icons.pause, size: 50, color: Colors.black,);
+                  });
+                  print('ritwik'+upload.audioURL);
+                  await player.play(upload.audioURL);
+                  setState(() {
+                    play=play=Icon(Icons.play_arrow, size: 50, color: Colors.black,);
+                  });
+                },
+              )
             ],
           ),
         ),
